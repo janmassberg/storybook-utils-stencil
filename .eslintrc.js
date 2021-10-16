7ecc7c8673390438d4c8bfb535b08b83deddc9fb
Jan Massberg <jan.massberg@odt.net>
2021-10-16 13:10:12 +0200

    chore: update eslint config for better react support

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,19 @@ module.exports = {
     parser: "babel-eslint",
     parserOptions: {
         project: "./tsconfig.json",
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-    plugins: ["@typescript-eslint", "jest"],
+    plugins: ["@typescript-eslint", "react", "jest"],
     extends: ["eslint:recommended", "plugin:react/recommended"],
+    settings: {
+        react: {
+            pragma: "React",
+            fragment: "Fragment",
+            version: "detect",
+        },
+    },
     rules: {
         "@typescript-eslint/no-unused-vars": [
             "error",
