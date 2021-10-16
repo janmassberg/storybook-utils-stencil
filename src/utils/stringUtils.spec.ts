5bf2732947eb076630d5d9d30c1e22bd05f4ac61
Jan Massberg <jan.massberg@odt.net>
2021-10-16 12:08:16 +0200

    test(utils): add unit tests for ucFirst and toQuotedKeyName string helper functions

--- src/utils/stringUtils.spec.ts
+++ src/utils/stringUtils.spec.ts
@@ -1,11 +1,34 @@
-import { toCamelCase, toKebabCase } from "./stringUtils";
+import {
+    toCamelCase,
+    toKebabCase,
+    ucFirst,
+    toQuotedKeyName,
+} from "./stringUtils";
 
 describe("toCamelCase", () => {
     it("converts string to camel case correctly", () => {
         expect(toCamelCase("my-example-string")).toEqual("myExampleString");
     });
+});
 
+describe("toKebabCase", () => {
     it("converts string to kebab case correctly", () => {
         expect(toKebabCase("myExampleString")).toEqual("my-example-string");
+    });
+});
+
+describe("ucFirst", () => {
+    it("converts the first letter to uppercase", () => {
+        expect(ucFirst("myExampleString")).toEqual("MyExampleString");
+    });
+});
+
+describe("toQuotedKeyName", () => {
+    it("does not add quotes if the string is a valid key name", () => {
+        expect(toQuotedKeyName("myExampleKey")).toEqual("myExampleKey");
+    });
+
+    it("adds quotes if the string contains invalid characters", () => {
+        expect(toQuotedKeyName("my-example-key")).toEqual(`"my-example-key"`);
     });
 });
