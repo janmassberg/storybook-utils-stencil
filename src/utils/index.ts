22be05313cee97d3224f0e0fb15ed71483aa6018
Jan Massberg <jan.massberg@odt.net>
2021-10-18 08:34:51 +0200

    chore: add utils index.ts

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export { formatHtml } from "./formatHtml";
+export { formatJsx } from "./formatJsx";
+export { objectToString } from "./objectToString";
+export { objectToStringJsx } from "./objectToStringJsx";
+export * from "./argUtils";
+export * from "./stringUtils";
