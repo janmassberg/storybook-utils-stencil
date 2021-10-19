33ebac9a74c7347f3946b070280cd3e6a88f2b81
Jan Massberg <jan.massberg@odt.net>
2021-10-19 08:37:05 +0200

    test(utils): outsource test args to src/utils/test-utils

---
+++ src/utils/test-utils/testArgs.ts
@@ -0,0 +1,15 @@
+export const testArgs = {
+    testNull: null,
+    testUndefined: undefined,
+    testFalse: false,
+    testTrue: true,
+    testString: "test-string",
+    testNumber: 12345,
+    testArray: [1, 2, 3],
+    testObject: {
+        foo: "bar",
+    },
+    testFunction: () => {},
+    onTestEvent: () => {},
+    _children: "Test children",
+};
