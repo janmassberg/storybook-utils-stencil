b213cf95d98cca804ccbbf52c7425ad94a356dc5
Jan Massberg <jan.massberg@odt.net>
2021-10-20 10:18:59 +0200

    chore: export generateSourceCodeReact in utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export { formatHtml } from "./formatHtml";
 export { formatJsx } from "./formatJsx";
 export { generateSourceCodeHtml } from "./generateSourceCodeHtml";
 export { generateSourceCodeVanillaJs } from "./generateSourceCodeVanillaJs";
+export { generateSourceCodeReact } from "./generateSourceCodeReact";
 export { objectToString } from "./objectToString";
 export { objectToStringJsx } from "./objectToStringJsx";
