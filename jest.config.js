7916377266ffd6ccdab44c062b739b997276a18a
Jan Massberg <jan.massberg@odt.net>
2021-10-20 08:15:31 +0200

    chore: add @testing-library/jest-dom dev dependency, create jest setup folder, update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,27 @@
 module.exports = {
-    roots: ["<rootDir>/src"],
-    testMatch: [
+    "roots": ["<rootDir>/src"],
+    "setupFilesAfterEnv": ['<rootDir>/.jest/jest.setup.js'],
+    "testMatch": [
         "**/__tests__/**/*.+(ts|tsx|js)",
         "**/?(*.)+(spec|test|e2e).+(ts|tsx|js)",
     ],
-    transform: { "^.+\\.(ts|tsx)$": "ts-jest" },
+    "transform": { "^.+\\.(ts|tsx)$": "ts-jest" },
+    "collectCoverage": true,
+    "collectCoverageFrom": [
+        "src/**/*.tsx?"
+    ],
+    "coveragePathIgnorePatterns": [
+        "node_modules",
+        "<rootDir>/src/test-utils"
+    ],
+    "coverageDirectory": "<rootDir>/coverage/",
+    "coverageThreshold": {
+        "global": {
+            "branches": 90,
+            "functions": 90,
+            "lines": 75,
+            "statements": 90
+        }
+    },
+    "mapCoverage": true
 };
