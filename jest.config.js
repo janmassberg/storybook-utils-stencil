b2f050a63925b486724d498d4692d14df1c18096
Jan Massberg <jan.massberg@odt.net>
2021-10-20 10:15:47 +0200

    chore: update package.json, update jest config, remove unused jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,20 @@
 module.exports = {
-    "roots": ["<rootDir>/src"],
-    "setupFilesAfterEnv": ['<rootDir>/.jest/jest.setup.js'],
-    "testMatch": [
+    roots: ["<rootDir>/src"],
+    testMatch: [
         "**/__tests__/**/*.+(ts|tsx|js)",
         "**/?(*.)+(spec|test|e2e).+(ts|tsx|js)",
     ],
-    "transform": { "^.+\\.(ts|tsx)$": "ts-jest" },
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-        "src/**/*.tsx?"
-    ],
-    "coveragePathIgnorePatterns": [
-        "node_modules",
-        "<rootDir>/src/test-utils"
-    ],
-    "coverageDirectory": "<rootDir>/coverage/",
-    "coverageThreshold": {
-        "global": {
-            "branches": 90,
-            "functions": 90,
-            "lines": 75,
-            "statements": 90
-        }
+    transform: { "^.+\\.(ts|tsx)$": "ts-jest" },
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.tsx?"],
+    coveragePathIgnorePatterns: ["node_modules", "<rootDir>/src/test-utils"],
+    coverageDirectory: "<rootDir>/coverage/",
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 90,
+            lines: 75,
+            statements: 90,
+        },
     },
-    "mapCoverage": true
 };
