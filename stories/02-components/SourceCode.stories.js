22719ad95e4d5e7ce11f303c45547913f03becbc
Jan Massberg <mail@janmassberg.de>
2024-05-11 18:47:29 +0200

    docs: add SourceCode component story with all supported frameworks

--- stories/02-components/SourceCode.stories.js
+++ stories/02-components/SourceCode.stories.js
@@ -5,8 +5,12 @@ import {
     themes,
 } from "@storybook/theming";
 import {
-    generateSourceCodeHtml,
+    generateSourceCodeAngularComponent,
+    generateSourceCodeAngularHtml,
     generateSourceCodeJavascript,
+    generateSourceCodeVue,
+    generateSourceCodeReact,
+    generateSourceCodeHtml,
     SourceCode,
 } from "../../src";
 import { testArgs } from "../../src/test-utils/testArgs";
@@ -96,6 +100,54 @@ export const MultipleTabs = {
                 {
                     language: "javascript",
                     code: "const element = document.querySelector('my-component#test-id');",
+                },
+            ],
+        },
+    },
+};
+
+export const AllFrameworks = {
+    args: {
+        codeBlocks: {
+            VanillaJS: [
+                {
+                    language: "html",
+                    code: generateSourceCodeHtml("my-component", testArgs),
+                },
+                {
+                    language: "html",
+                    code: generateSourceCodeJavascript(
+                        "my-component",
+                        testArgs
+                    ),
+                },
+            ],
+            Angular: [
+                {
+                    language: "html",
+                    code: generateSourceCodeAngularHtml(
+                        "my-component",
+                        testArgs
+                    ),
+                },
+                {
+                    language: "typescript",
+                    code: generateSourceCodeAngularComponent(
+                        "my-component",
+                        testArgs
+                    ),
+                },
+            ],
+            React: [
+                {
+                    language: "typescript",
+                    code: generateSourceCodeReact("my-component", testArgs),
+                },
+            ],
+            Vue: [
+                {
+                    language: "html",
+                    code: generateSourceCodeVue("my-component", testArgs),
                 },
             ],
         },
